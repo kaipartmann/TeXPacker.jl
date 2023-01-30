2bf90145e820f541af8ff20cc14f2a78c4d298a9
Documenter.jl <documenter@juliadocs.github.io>
2023-01-30 09:03:32 +0000

    build based on c755bd1

---
+++ dev/search_index.js
@@ -0,0 +1,3 @@
+var documenterSearchIndex = {"docs":
+[{"location":"","page":"Home","title":"Home","text":"CurrentModule = TeXPacker","category":"page"},{"location":"#TeXPacker","page":"Home","title":"TeXPacker","text":"","category":"section"},{"location":"","page":"Home","title":"Home","text":"Documentation for TeXPacker.","category":"page"},{"location":"","page":"Home","title":"Home","text":"","category":"page"},{"location":"","page":"Home","title":"Home","text":"Modules = [TeXPacker]","category":"page"}]
+}
